8905bec346ae1bae06a15f26a18de6d54b908aec
Joje1283 <joje1283@gmail.com>
2021-04-01 01:03:57 +0900

    변수 인라인하기(6.4절) - 리팩터링한 코드에서 오히려 조회수가 많아진다. 하지만 성능 개선이 수월.

--- chapter_01/statement.py
+++ chapter_01/statement.py
@@ -26,13 +26,11 @@ def statement(invoice):
     result = f'청구 내역 (고객명: {invoice["customer"]})\n'
 
     for performance in invoice['performances']:
-        this_amount = amount_for(performance)
-
         volume_credits += max(performance['audience'] - 30, 0)
         if 'comedy' == play_for(performance)['type']:
             volume_credits += floor(performance['audience'] / 5)
-        result += f' {play_for(performance)["name"]}: {this_amount / 100} ({performance["audience"]}석)\n'
-        total_amount += this_amount
+        result += f' {play_for(performance)["name"]}: {amount_for(performance) / 100} ({performance["audience"]}석)\n'
+        total_amount += amount_for(performance)
     result += f'총액: {total_amount/100}\n'
     result += f'적립 포인트: {volume_credits}점\n'
     return result
