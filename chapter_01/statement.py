702dc64897cd617f7020a396facfa48cbb39c221
Joje1283 <joje1283@gmail.com>
2021-04-02 00:04:37 +0900

    total_amount 변수 제거하기 (p48) 중간 점검: 난무하는 중첩함수 (p50)

--- chapter_01/statement.py
+++ chapter_01/statement.py
@@ -1,54 +1,53 @@
 from math import floor
 
 
-def amount_for(a_performance):
-    if play_for(a_performance)['type'] == 'tragedy':
-        result = 40000
-        if a_performance['audience'] > 30:
-            result += 1000 * (a_performance['audience'] - 30)
-    elif play_for(a_performance)['type'] == 'comedy':
-        result = 30000
-        if a_performance['audience'] > 20:
-            result += 10000 + 500 * (a_performance['audience'] - 20)
-        result += 300 * a_performance['audience']
-    else:
-        raise Exception(f'알수 없는 장르 {play_for(a_performance)["type"]}')
-    return result
-
-
-def play_for(a_performance):
-    return plays[a_performance['playID']]
-
-
-def volume_credits_for(a_performance):
-    result = 0
-    result += max(a_performance['audience'] - 30, 0)
-    if 'comedy' == play_for(a_performance)['type']:
-        result += floor(a_performance['audience'] / 5)
-    return result
-
-
-def total_volume_credits(performances):
-    volume_credits = 0
-    for performance in performances:
-        volume_credits += volume_credits_for(performance)
-    return volume_credits
-
-
-def usd(a_number):
-    return f'${round(a_number/100, 2):,}'
-
-
 def statement(invoice):
-    total_amount = 0
+    def total_amount():
+        result = 0
+        for performance in invoice['performances']:
+            result += amount_for(performance)
+        return result
+
+    def total_volume_credits():  # 중첩함수, 매개변수 전달 필요 X
+        result = 0
+        for performance in invoice["performances"]:
+            result += volume_credits_for(performance)
+        return result
+
+    def usd(a_number):
+        return f'${round(a_number / 100, 2):,}'
+
+    def volume_credits_for(a_performance):
+        result = 0
+        result += max(a_performance['audience'] - 30, 0)
+        if 'comedy' == play_for(a_performance)['type']:
+            result += floor(a_performance['audience'] / 5)
+        return result
+
+    def play_for(a_performance):
+        return plays[a_performance['playID']]
+
+    def amount_for(a_performance):
+        if play_for(a_performance)['type'] == 'tragedy':
+            result = 40000
+            if a_performance['audience'] > 30:
+                result += 1000 * (a_performance['audience'] - 30)
+        elif play_for(a_performance)['type'] == 'comedy':
+            result = 30000
+            if a_performance['audience'] > 20:
+                result += 10000 + 500 * (a_performance['audience'] - 20)
+            result += 300 * a_performance['audience']
+        else:
+            raise Exception(f'알수 없는 장르 {play_for(a_performance)["type"]}')
+        return result
+
     result = f'청구 내역 (고객명: {invoice["customer"]})\n'
 
     for performance in invoice['performances']:
         result += f' {play_for(performance)["name"]}: {usd(amount_for(performance))} ({performance["audience"]}석)\n'
-        total_amount += amount_for(performance)
 
-    result += f'총액: {usd(total_amount)}\n'
-    result += f'적립 포인트: {total_volume_credits(invoice["performances"])}점\n'
+    result += f'총액: {usd(total_amount())}\n'
+    result += f'적립 포인트: {total_volume_credits()}점\n'
     return result
 
 
