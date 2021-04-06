9eb9a09703c4c63d647f9ea08f284d237e45684a
Joje1283 <joje1283@gmail.com>
2021-04-07 00:56:41 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기 - html버전 작성

--- chapter_01/statement.py
+++ chapter_01/statement.py
@@ -1,14 +1,15 @@
 from create_statement_data import create_statement_data
 
 
+def usd(a_number):
+    return f'${round(a_number / 100, 2):,}'
+
+
 def statement(invoice, plays):
-    return render_plain_text(create_statement_data(invoice, plays), plays)
+    return render_plain_text(create_statement_data(invoice, plays))
 
 
-def render_plain_text(data, plays):
-    def usd(a_number):
-        return f'${round(a_number / 100, 2):,}'
-
+def render_plain_text(data):
     result = f'청구 내역 (고객명: {data["customer"]})\n'
 
     for performance in data['performances']:
@@ -16,6 +17,23 @@ def render_plain_text(data, plays):
 
     result += f'총액: {usd(data["total_amount"])}\n'
     result += f'적립 포인트: {data["total_volume_credits"]}점\n'
+    return result
+
+
+def html_statement(invoice, plays):
+    return render_html(create_statement_data(invoice, plays))
+
+
+def render_html(data):
+    result = f'<h1>청구 내욕 (고객명: ${data["customer"]}</h1>\n'
+    result += '<table>\n'
+    result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>'
+    for performance in data['performances']:
+        result += f'  <tr><td>{performance["play"]["name"]}</td><td>({performance["audience"]}석)</td>'
+        result += f'<td>{usd(performance["amount"])}</td></tr>\n'
+    result += "</table>\n"
+    result += f'<p>총액: <em>{usd(data["total_amount"])}</em></p>\n'
+    result += f'<p>적립 포인트: <em>{data["total_volume_credits"]}</em>점</p>\n'
     return result
 
 
@@ -39,7 +57,8 @@ if __name__ == '__main__':
 
     invoice = invoices[0]
 
-    print(statement(invoice, plays))
+    # print(statement(invoice, plays))
+    print(html_statement(invoice, plays))
     """ 출력 결과
     청구 내역 (고객명: BigCo)
      Hamlet: $650.0 (55석)
