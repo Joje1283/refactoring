d13e67383cbbed1a24df00ce0a3af2225c40a969
Joje1283 <joje1283@gmail.com>
2021-04-07 00:41:32 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기 - create_statement_data를 별도의 파일로 분리

--- chapter_01/statement.py
+++ chapter_01/statement.py
@@ -1,55 +1,8 @@
-from functools import reduce
-from math import floor
-import copy
+from create_statement_data import create_statement_data
 
 
 def statement(invoice, plays):
     return render_plain_text(create_statement_data(invoice, plays), plays)
-
-
-def create_statement_data(invoice, plays):  # 중간 데이터 생성을 전담
-    def play_for(a_performance):
-        return plays[a_performance['playID']]
-
-    def amount_for(a_performance):
-        if a_performance['play']['type'] == 'tragedy':
-            result = 40000
-            if a_performance['audience'] > 30:
-                result += 1000 * (a_performance['audience'] - 30)
-        elif a_performance['play']['type'] == 'comedy':
-            result = 30000
-            if a_performance['audience'] > 20:
-                result += 10000 + 500 * (a_performance['audience'] - 20)
-            result += 300 * a_performance['audience']
-        else:
-            raise Exception(f'알수 없는 장르 {a_performance["play"]["type"]}')
-        return result
-
-    def volume_credits_for(a_performance):
-        result = 0
-        result += max(a_performance['audience'] - 30, 0)
-        if 'comedy' == a_performance['play']['type']:
-            result += floor(a_performance['audience'] / 5)
-        return result
-
-    def total_amount(data):
-        return reduce(lambda total, performance: total + performance['amount'], data['performances'], 0)
-
-    def total_volume_credits(data):
-        return reduce(lambda total, performance: total + performance['volume_credits'], data['performances'], 0)
-
-    def enrich_performance(performances):
-        result = copy.copy(performances)
-        for performance in result:
-            performance['play'] = play_for(performance)
-            performance['amount'] = amount_for(performance)
-            performance['volume_credits'] = volume_credits_for(performance)
-        return result
-
-    statement_data = {'customer': invoice['customer'], 'performances': enrich_performance(invoice['performances'])}
-    statement_data['total_amount'] = total_amount(statement_data)
-    statement_data['total_volume_credits'] = total_volume_credits(statement_data)
-    return statement_data
 
 
 def render_plain_text(data, plays):
