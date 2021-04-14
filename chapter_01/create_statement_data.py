a7ca0e532f73839f757fa028821b4ad88f1d64eb
Joje1283 <joje1283@gmail.com>
2021-04-15 07:58:07 +0900

    팩터리함수를 멤버함수에서 일반함수로 분리

--- chapter_01/create_statement_data.py
+++ chapter_01/create_statement_data.py
@@ -10,15 +10,6 @@ class PerformanceCalculator(metaclass=ABCMeta):  # 추상 클래스
         self.performance = performance
         self.play = play
 
-    @classmethod
-    def create_performance_calculator(cls, performance, play):  # 생성자 대신 팩터리 함수를 이용한 인스턴스 반환
-        if play['type'] == 'tragedy':
-            return TragedyCalculator(performance, play)
-        elif play['type'] == 'comedy':
-            return ComedyCalculator(performance, play)
-        else:
-            raise Exception(f'알 수 없는 장르: {play["type"]}')
-
     @abstractmethod
     def amount(self):
         pass
@@ -29,10 +20,6 @@ class PerformanceCalculator(metaclass=ABCMeta):  # 추상 클래스
 
 
 class TragedyCalculator(PerformanceCalculator):
-    def __init__(self, *args, **kwargs):
-        result = super().__init__(*args, **kwargs)
-        print('TragedyCalculator')
-
     @property
     def amount(self):  # 추상 메서드
         result = 40000
@@ -42,10 +29,6 @@ class TragedyCalculator(PerformanceCalculator):
 
 
 class ComedyCalculator(PerformanceCalculator):
-    def __init__(self, *args, **kwargs):
-        result = super(ComedyCalculator, self).__init__(*args, **kwargs)
-        print('ComedyCalculator')
-
     @property
     def amount(self):  # 추상 메서드
         result = 30000
@@ -61,6 +44,15 @@ class ComedyCalculator(PerformanceCalculator):
         return result
 
 
+def create_performance_calculator(performance, play):  # 생성자 대신 팩터리 함수를 이용한 인스턴스 생성
+    if play['type'] == 'tragedy':
+        return TragedyCalculator(performance, play)
+    elif play['type'] == 'comedy':
+        return ComedyCalculator(performance, play)
+    else:
+        raise Exception(f'알 수 없는 장르: {play["type"]}')
+
+
 def create_statement_data(invoice, plays):
     def play_for(a_performance):
         return plays[a_performance['playID']]
@@ -72,7 +64,7 @@ def create_statement_data(invoice, plays):
         return reduce(lambda total, performance: total + performance['volume_credits'], data['performances'], 0)
 
     def enrich_performance(performance):
-        calculator = PerformanceCalculator.create_performance_calculator(performance, play_for(performance))
+        calculator = create_performance_calculator(performance, play_for(performance))
         result = copy.copy(performance)
         result['play'] = calculator.play  # 공연 이름 및 타입
         result['amount'] = calculator.amount  # 공연 견적 (가격)
