6bdc47c659fc88071a32f6a4f7a1e4cdd2900baa
joje1283 <joje1283@gmail.com>
2021-04-28 01:11:48 +0900

    6.3. 변수 추출하기 일반 예시 - 추출하기 예시

--- chapter_06/3. 변수 추출하기.py
+++ chapter_06/3. 변수 추출하기.py
@@ -1,11 +1,12 @@
 def price(order):
-    return order['quantity'] * order['item_price'] - \
-           max(0, order['quantity'] - 500) * order['item_price'] * 0.05 + \
-           min(order['quantity'] * order['item_price'] * 0.1, 100)
+    base_price = order['quantity'] * order['item_price']
+    quantity_discount = max(0, order['quantity'] - 500) * order['item_price'] * 0.05
+    shipping = min(base_price * 0.1, 100)
+    return base_price - quantity_discount + shipping
 
 
 if __name__ == '__main__':
     print(price({
         'quantity': 10,
-        'item_price': 1000,
+        'item_pric시e': 1000,
     }))
