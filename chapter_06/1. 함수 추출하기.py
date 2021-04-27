b9a85f0f687171cde16add1b623cf2ef1710245b
joje1283 <joje1283@gmail.com>
2021-04-28 00:34:16 +0900

    6.1. 함수 추출하기 예시2 - 지역 변수를 사용할 때 (다른 값을 다시 대입할때와 대입하지 않을 때)

--- chapter_06/1. 함수 추출하기.py
+++ chapter_06/1. 함수 추출하기.py
@@ -8,11 +8,6 @@ def print_owing(invoice):
         print("**** 고객 채무 ****")
         print("*****************")
 
-    def print_details():
-        print(f'고객명: {invoice["customer"]}')
-        print(f'채무액: {outstanding}')
-        print(f'마감일: {invoice["due_date"].strftime("%Y-%m-%d")}')
-
     outstanding = 0
     print_banner()
 
@@ -21,10 +16,19 @@ def print_owing(invoice):
         outstanding += o['amount']
 
     # 마감일(dueDate)을 기록한다.
+    record_due_date(invoice)
+    # 세부 사항을 출력한다.
+    print_details(invoice, outstanding)
+
+
+def record_due_date(invoice):
     invoice['due_date'] = datetime.now() + timedelta(days=30)
 
-    # 세부 사항을 출력한다.
-    print_details()
+
+def print_details(invoice, outstanding):
+    print(f'고객명: {invoice["customer"]}')
+    print(f'채무액: {outstanding}')
+    print(f'마감일: {invoice["due_date"].strftime("%Y-%m-%d")}')
 
 
 if __name__ == '__main__':
