0a0ba6addad91b3eb327112874b0205aefd6711d
joje1283 <joje1283@gmail.com>
2021-04-29 00:25:55 +0900

    6.5. 함수 선언 출 - 함수 추

--- chapter_06/5. 함수 선언 바꾸기.py
+++ chapter_06/5. 함수 선언 바꾸기.py
@@ -1,5 +1,9 @@
 def in_new_england(customer):
     state_code = customer['address']['state']
+    return xx_in_new_england(state_code)
+
+
+def xx_in_new_england(state_code):
     return state_code in ["MA", "CT", "ME", "VT", "NH", "RI"]
 
 
