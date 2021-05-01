1f66386a459977cd86e88b4f27ef8a903c8f3aa6
joje1283 <joje1283@gmail.com>
2021-05-01 14:35:33 +0900

    6.6. 변수 캡슐화 하기. - 레코드 캡슐화하기를 이용해서 원본 수정은 가능하되, 다른 객체간에는 영향을 받지 않도록 함

--- chapter_06/playground.py
+++ chapter_06/playground.py
@@ -4,8 +4,30 @@ default_owner = {'first_name': '마틴', 'last_name': '파울러'}
 
 
 def get_default_owner():
-    return copy.deepcopy(default_owner)
+    return Person(default_owner)
 
 
 def set_default_owner(arg):
     default_owner = arg
+
+
+class Person:
+    def __init__(self, data):
+        self._last_name = data['last_name']
+        self._first_name = data['first_name']
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, arg):
+        self._last_name = arg
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, arg):
+        self._first_name = arg
