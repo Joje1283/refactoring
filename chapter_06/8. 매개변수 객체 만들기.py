685d50df0b02f89c575b96bde64e77429ed09942
joje1283 <joje1283@gmail.com>
2021-05-02 14:43:24 +0900

    6.8. 매개변수 객체 만들기 - 매개변수 객체 만들기 적용

--- chapter_06/8. 매개변수 객체 만들기.py
+++ chapter_06/8. 매개변수 객체 만들기.py
@@ -16,12 +16,26 @@ operation_plan = {
 }
 
 
+class NumberRange:
+    def __init__(self, min, max):
+        self._data = {'min': min, 'max': max}
+
+    @property
+    def min(self):
+        return self._data['min']
+
+    @property
+    def max(self):
+        return self._data['max']
+
+
 # 정상 범위를 벗어난 측정값을 찾는 함수
-def readings_outside_range(station, min, max):
-    return list(filter(lambda x: x['temp'] < min or x['temp'] > max, station['readings']))
+def readings_outside_range(station, range=None):
+    return list(filter(lambda x: x['temp'] < range.min or x['temp'] > range.max, station['readings']))
 
 
 # 호출문
 if __name__ == '__main__':
-    alerts = readings_outside_range(station, operation_plan['temperature_floor'], operation_plan['temperature_ceiling'])
+    range = NumberRange(operation_plan['temperature_floor'], operation_plan['temperature_ceiling'])
+    alerts = readings_outside_range(station, range)
     print(alerts)
